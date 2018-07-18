0c3912e12ea5378f746cb9b7848ac5493caa023b
Mátyus Botond <matyusbotond@outlook.hu>
2018-07-18 10:21:47 +0200

    list person

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -10,46 +10,78 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            List<string> list1 = new List<string>();
+            //List<string> list1 = new List<string>();
 
-            //Listához hozzáadás
-            list1.Add("alma");
-            list1.Add("korte");
-            list1.Add("barack");
+            ////Listához hozzáadás
+            //list1.Add("alma");
+            //list1.Add("korte");
+            //list1.Add("barack");
 
-            //Adott indexre beszúrás
-            list1.Insert(1, "meggy");
+            ////Adott indexre beszúrás
+            //list1.Insert(1, "meggy");
 
-            foreach (string item in list1)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (string item in list1)
+            //{
+            //    Console.WriteLine(item);
+            //}
             
-            bool contains = list1.Contains("alma");
+            //bool contains = list1.Contains("alma");
 
-            if (list1.Contains("alma"))
+            //if (list1.Contains("alma"))
+            //{
+            //    list1.Remove("alma");
+            //}
+
+            //int index = list1.IndexOf("korte");
+            //int index2 = list1.IndexOf("korte2");
+
+            ////Adott elem törlése
+            //bool removeResult = list1.Remove("alma");
+            ////bool removeResult2 = list1.Remove("alma2");
+
+            ////Adott indexen lévő elem törlése
+            //list1.RemoveAt(0);
+
+
+            //foreach (string item in list1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            ////Összes elem törlése
+            //list1.Clear();
+
+            //Console.ReadKey();
+
+            List<Person> people = new List<Person>();
+
+            Person p1 = new Person();
+
+            p1.Name = "Béla";
+
+            people.Add(p1);
+
+            Person p2 = new Person();
+            p2.Name = "Aladár";
+
+            people.Add(p2);
+
+            foreach (Person person in people)
             {
-                list1.Remove("alma");
+                Console.WriteLine(person.Name);
             }
 
-            int index = list1.IndexOf("korte");
-            int index2 = list1.IndexOf("korte2");
-
-            //Adott elem törlése
-            bool removeResult = list1.Remove("alma");
-            //bool removeResult2 = list1.Remove("alma2");
-
-            //Adott indexen lévő elem törlése
-            list1.RemoveAt(0);
-
-
-            foreach (string item in list1)
+            if (people.Contains(p1))
             {
-                Console.WriteLine(item);
+                people.Remove(p1);
             }
 
-            //Összes elem törlése
-            list1.Clear();
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            Person aladar = people[0];
 
             Console.ReadKey();
         }
